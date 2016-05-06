914d8d16038fb1368a7e7ce53d8f25ece48270b2
IGGAMEMAKER <23i03g@mail.ru>
2016-05-06 16:53:58 +0300

    todo2

--- src/app.js
+++ src/app.js
@@ -1,15 +1,8 @@
-import Timer from './Timer';
+import TodoList from './TodoList';
 import React from 'react';
 import { render } from 'react-dom';
 
-import Pack from './Pack';
-
 render(
-  <Timer />,
-  document.getElementById('app')
-)
-
-render(
-  <Pack />,
-  document.getElementById('packs')
+	<TodoList />,
+    document.getElementById('app')
 )
